f0147bf532290fc08a7d8f9a0ae6f644ab6aa2e8
Summer Salad <summer.salad.games@gmail.com>
2025-04-21 21:00:02 +0200

    adding gamelay

--- src/dice/main.py
+++ src/dice/main.py
@@ -18,12 +18,15 @@ class Main:
     ):
         logging.info("Initializing program")
 
+        self._is_loading = True
         self._init_delay = init_delay
         self._loop_delay = loop_delay
 
-        self.dataPin = OutputDevice(data_pin)
-        self.latchPin = OutputDevice(latch_pin)
-        self.clockPin = OutputDevice(clock_pin)
+        self._data_pin = OutputDevice(data_pin)
+        self._latch_pin = OutputDevice(latch_pin)
+        self._clock_pin = OutputDevice(clock_pin)
+
+        self._button = Button(5, bounce_time=0.1)
 
         sleep(self._init_delay)
 
@@ -31,9 +34,9 @@ class Main:
         logging.info("Setting up program")
         self._reset()
 
-        self._button = Button(5, bounce_time=0.1)
         self._button.when_activated = lambda: self._button_event(True)
         self._button.when_deactivated = lambda: self._button_event(False)
+        self._is_loading = False
 
     def _loop(self):
         sleep(self._loop_delay)
@@ -44,42 +47,62 @@ class Main:
 
     def _reset(self):
         logging.info("Resetting program")
-        self._shift_out(0xFF)
-        self.dataPin.off()
-        self.latchPin.off()
-        self.clockPin.off()
+        self._clear_screen()
+        self._data_pin.off()
+        self._latch_pin.off()
+        self._clock_pin.off()
+        self._is_loading = False
+
+    def _clear_screen(self):
+        self._shift_out(self._BLANK)
 
     def _shift_out(self, data):
         sleep(0.01)
-        self.latchPin.off()
+        self._latch_pin.off()
         for i in range(8):
             if (data >> i) & 0b1:
-                self.dataPin.on()
+                self._data_pin.on()
             else:
-                self.dataPin.off()
+                self._data_pin.off()
 
-            self.clockPin.on()
+            self._clock_pin.on()
             sleep(0.01)
-            self.clockPin.off()
+            self._clock_pin.off()
 
         sleep(0.01)
-        self.latchPin.on()
+        self._latch_pin.on()
         sleep(0.01)
-        self.latchPin.off()
+        self._latch_pin.off()
         sleep(0.01)
 
     def _show_number(self, number):
         if number < 0 or number > 9:
-            self._shift_out(self._BLANK)
+            self._clear_screen()
         else:
             self._shift_out(self._NUMBERS[number])
 
     def _button_event(self, pressed):
-        logging.info(f"Button pressed: {pressed}")
-        if pressed:
+        if pressed and not self._is_loading:
+            logging.info(f"Button pressed: {pressed}")
+            self._is_loading = True
             number = randint(0, 9)
-            logging.info(f"Showing number: {number}")
+            logging.info(f"Chosen number: {number}")
+
+            delay = 0.005
+            for i in range(9):
+                temporary_number = randint(0, 9)
+                self._show_number(temporary_number)
+                sleep(delay)
+                delay += delay
+
+            for i in range(5):
+                self._show_number(number)
+                sleep(0.5)
+                self._clear_screen()
+                sleep(0.5)
+
             self._show_number(number)
+            self._is_loading = False
 
     def start(self):
         logging.info("Starting program")
